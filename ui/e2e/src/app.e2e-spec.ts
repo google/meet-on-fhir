b82c30772d26786d2233ae37d59549d893b9868a
Biao She <shebiao@gmail.com>
2020-08-31 16:37:28 -0400

    Rename waiting-room-ui folder to ui

--- ui/e2e/src/app.e2e-spec.ts
+++ ui/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('waiting-room-ui app is running!');
+    expect(page.getTitleText()).toEqual('ui app is running!');
   });
 
   afterEach(async () => {
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'waiting-room-ui';
+  title = 'ui';
 }
